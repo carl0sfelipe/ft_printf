7c8cc2629e40813f527d54cab6308dfd185260b2
carl0sfelipe <carlos.felipe@hotmail.com.br>
2022-07-02 21:08:39 -0300

    bug

--- ft_printf.c
+++ ft_printf.c
@@ -3,28 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: csiqueir <csiqueir@student.42.rio>         +#+  +:+       +#+        */
+/*   By: csiqueir <carlos.felipe@hotmail.com.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 20:07:47 by csiqueir          #+#    #+#             */
-/*   Updated: 2022/06/30 20:44:51 by csiqueir         ###   ########.fr       */
+/*   Updated: 2022/07/02 20:57:24 by csiqueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
+#include <string.h>
 #include <stdarg.h>
+#include <unistd.h>
+
+
+int	ft_putstr(char *s)
+{
+/*	int	i;
+
+	i = 0;*/
+	if (!s)
+		return 0;
+	/*while (s[i])
+	{*/
+		write(1,s, strlen(s));
+	/*	i++;
+	}*/
+	return strlen(s);
+}
+
 
 int ft_printf(const char *output, ...)
 {
 	va_list args;
+	int variable;
+	int index;
+
+	index = 0;
+	variable = 0;
 	va_start(args, output);
-	
-	char ch = va_arg(args, int);
-	putchar(ch);
-	
-	return strlen((const char *)ch);
+	while (output[index])
+	{
+		if (output[index] != '%')
+		{
+			putchar(output[index]);
+			variable++;
+			index++;
+		}
+		else
+		{
+			index++;
+			if (output[index] == 'c')
+			{
+				putchar(va_arg(args, int));
+				variable++;
+				index++;
+			}
+			else if (output[index] == 's')
+			{
+				variable += ft_putstr(va_arg(args, char *));
+				index++;
+			}
+		}
+	}
 	va_end(args);
+	return (variable);
 }
 int main()
 {
-	ft_printf("Printing a character: '%c'",'A');
+	ft_printf("Printing characters: '%c' '%s' '%c' '%s'", 'A', "AA",'B',"BB");
 	return 0;
 }
